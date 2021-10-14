15db8658e3649805fecdf6fc7be05be46444472e
Tristan Mouritsen <tmouritsen57@gmail.com>
2021-10-14 13:18:09 -0600

    reverted to 31c9c8fb342a22081904fc0c94de7d0d2720ae31 for my_file.py

---
+++ demo_file.py
@@ -0,0 +1,14 @@
+﻿some new stuff
+more new stuff
+
+newest newer stuff
+newest newer stuff
+newest newer stuff
+newest newer stuff
+oops this change might conflict??
+oops this change might conflict??
+oops this change might conflict??
+oops this change might conflict??
+oops this change might conflict??
+
+print("update python function with merge conflict");
--- my_file.py
+++ my_file.py
@@ -2,13 +2,5 @@
 more new stuff
 
 newest newer stuff
-newest newer stuff
-newest newer stuff
-newest newer stuff
-oops this change might conflict??
-oops this change might conflict??
-oops this change might conflict??
-oops this change might conflict??
-oops this change might conflict??
 
 print("update python function with merge conflict");
